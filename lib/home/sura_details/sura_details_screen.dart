9a1cb45bff059a29328f850c8b40429e37d3e795
SARA-KHATAB\Free-Comp <sara.khatab.bm@gmail.com>
2025-12-21 01:57:38 +0200

    sura details finished

--- lib/home/sura_details/sura_details_screen.dart
+++ lib/home/sura_details/sura_details_screen.dart
@@ -4,16 +4,27 @@ import 'package:islami_application/core/app_colors.dart';
 import 'package:islami_application/core/app_style.dart';
 import 'package:islami_application/home/model/sura_model.dart';
 
-class SuraDetailsScreen extends StatelessWidget {
+class SuraDetailsScreen extends StatefulWidget {
   static const String routeName = "SuraDetailsScreen";
-  List<String>verses=[];
 
   SuraDetailsScreen({super.key});
 
   @override
+  State<SuraDetailsScreen> createState() => _SuraDetailsScreenState();
+}
+
+class _SuraDetailsScreenState extends State<SuraDetailsScreen> {
+  List<String>verses = [];
+
+  @override
   Widget build(BuildContext context) {
-    var model = ModalRoute.of(context)?.settings.arguments as SuraModel;
-    LoadSuraFile(model.suraIndex);
+    var model = ModalRoute
+        .of(context)
+        ?.settings
+        .arguments as SuraModel;
+    if (verses.isEmpty) {
+      LoadSuraFile(model.suraIndex);
+    }
     return Scaffold(
       backgroundColor: AppColors.black,
       appBar: AppBar(
@@ -30,13 +41,17 @@ class SuraDetailsScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 35),
                 child: Text(model.nameAr, style: AppStyle.titlestyle),
               ),
-              SizedBox(height: 50 ,),
+              SizedBox(height: 50,),
               Expanded(
-                child: ListView.builder( itemCount: verses.length,
-                  itemBuilder: (context, index) => Center(child: Text(verses[index],style: AppStyle.bodyStyle,),),
+                child: ListView.builder(itemCount: verses.length,
+                  itemBuilder: (context, index) =>
+                      Center(child: Text(
+                        verses[index], style: AppStyle.titlestyle, textAlign
+                        :TextAlign.center,),
                 ),
               ),
-              SizedBox(height: 100,)
+    ),
+              SizedBox(height: 100,),
             ],
           ),
         ],
@@ -44,11 +59,12 @@ class SuraDetailsScreen extends StatelessWidget {
     );
   }
 
-Future<void>LoadSuraFile(int index)async{
-    String SuraFile= await rootBundle.loadString("assets/files/$index.txt");
-    List<String>SuraLine=[];
-    verses=SuraLine;
-
-
-}
+  Future<void> LoadSuraFile(int index) async {
+    String SuraFile = await rootBundle.loadString("assets/files/$index.txt");
+    List<String>SuraLine = SuraFile.split("/n");
+    verses = SuraLine;
+    setState(() {
+      print("verses");
+    });
+  }
 }
